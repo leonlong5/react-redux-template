611d69a3d386563408672a03d6779612c3802a2f
Leon Wang <long.wang56@gmail.com>
2019-05-19 17:58:42 -0700

    First Update

---
+++ src/components/robots.js
@@ -0,0 +1,38 @@
+
+export const robots = [
+    {
+        id: 1,
+        name: 'Frank',
+        email: 'frank@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Rose',
+        email: 'Rose@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Toby',
+        email: 'Toby@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Jack',
+        email: 'Jack@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Sunny',
+        email: 'Sunny@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Party-Joe',
+        email: 'Party-Joe@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Geek',
+        email: 'Geek@gmail.com'
+    },
+]
